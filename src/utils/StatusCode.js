2b1e0e2fdf3a7f241268882096fd3f2b7feb9ccc
Lavesh1208 <laveshs1282@gmail.com>
2023-08-15 21:04:30 +0530

    15 Aug, 2023

--- src/utils/StatusCode.js
+++
@@ -1,8 +0,0 @@
-// This is an object that defines different status codes, including loading, idle, and error.
-const StatusCode = {
-   LOADING: 'loading',
-   IDLE: 'idle',
-   ERROR: 'error'
-}
-
-export default StatusCode;
